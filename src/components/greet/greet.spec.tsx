f4c709bbce878d3d6eb0005e71829004c6f3345a
piyawatpm <piyawat_00101@hotmail.com>
2022-09-25 16:09:04 +1000

    xit and fit instead of test

--- src/components/greet/greet.spec.tsx
+++ src/components/greet/greet.spec.tsx
@@ -1,14 +1,16 @@
 import { render, screen } from '@testing-library/react'
 import { Greet } from './greet'
 // .only .skip
+// fit = test.only
+// xit = skip
 describe('Greet', () => {
-    it('renders correctly', () => {
+    xit('renders correctly', () => {
         render(<Greet />)
         const textElement = screen.getByText(/hello/i)
         expect(textElement).toBeInTheDocument()
     })
     describe('Nested', () => {
-        it('renders  with a name', () => {
+        fit('renders  with a name', () => { 
             render(<Greet name='piyawat' />)
             const textElement = screen.getByText('Hello piyawat')
             expect(textElement).toBeInTheDocument()
